f80653d6749c97c71b1917e31195e5bbbed1b8f7
Hoo <Hoo@163.com>
2019-01-16 20:28:25 +0800

    项目目录搭建，实现配置数据库、迁移框架、脚本管理器

--- manage.py
+++ manage.py
@@ -2,12 +2,19 @@ from flask import Flask,session
 
 from flask_session import Session
 
-from flask_sqlalchemy import SQLAlchemy
-
 from redis import StrictRedis
 
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_script import Manager
+
+from flask_migrate import Migrate,MigrateCommand
 
 app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://roou:mysql@localhost/info_python38'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 
 app.config['SECRET_KEY']='fE/S+iP1yeXBfEKRIL9mYCubw4AV6InrpByVbSPuz4L6MBANFL4DMg=='
 
@@ -21,6 +28,13 @@ app.config['SESSION_PERMENENT_LIFETIME']=86400
 
 Session(app)
 
+manage=Manager(app)
+
+db=SQLAlchemy(app)
+
+Migrate(app,db)
+
+manage.add_command('db',MigrateCommand)
 
 @app.route('/')
 def index():
@@ -28,4 +42,4 @@ def index():
     return 'hello world'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manage.run()
